09b95313e5867dec85c95b8e14f46731107ccc62
NoobCoder143 <162805971+NoobCoder143@users.noreply.github.com>
2025-04-09 16:38:03 +0530

    Deleted  db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const mongouri ="mongodb://localhost:27017"
-
-async function connectToMongo() {
-    await mongoose.connect(mongouri).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
-  }
-  module.exports = connectToMongo
